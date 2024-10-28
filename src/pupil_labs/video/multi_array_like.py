3f41b56e97b60903fde7712d4e4d1cffdc574c8e
Daniel Dourvaris <dourvaris@gmail.com>
2024-10-28 03:25:03 +0200

    Avoid calling len on parts in multi array unless necessary

--- src/pupil_labs/video/multi_array_like.py
+++ src/pupil_labs/video/multi_array_like.py
@@ -1,15 +1,18 @@
-from typing import Iterator, Sequence, overload
-
-import numpy as np
+from typing import Iterator, Sequence, Sized, overload
 
 from .array_like import ArrayLike, T
 from .reader import index_key_to_indices
 
 
+class LengthLookup(dict):
+    def __missing__(self, key: Sized) -> int:
+        return len(key)
+
+
 class MultiArrayLike(ArrayLike[T]):
-    def __init__(self, arrays: Sequence[ArrayLike[T]]) -> None:
-        self.arrays = arrays
-        self._start_indices = np.cumsum([0] + [len(part) for part in self.arrays])
+    def __init__(self, parts: Sequence[ArrayLike[T]]) -> None:
+        self.parts = parts
+        self.part_lengths = LengthLookup()
 
     @overload
     def __getitem__(self, key: int) -> T: ...
@@ -19,19 +22,20 @@ class MultiArrayLike(ArrayLike[T]):
     def __getitem__(self, key: int | slice) -> T | ArrayLike[T]:
         if isinstance(key, int):
             index = index_key_to_indices(key, self)[0]
-            if index >= len(self):
-                raise IndexError("Index out of range.")
+            offset = 0
+            for part in self.parts:
+                part_length = self.part_lengths[part]
+                part_index = index - offset
+                if part_index < part_length:
+                    return part[part_index]
+                offset += part_length
 
-            part_index = (
-                np.searchsorted(self._start_indices, index, side="right").item() - 1
-            )
-            part_key = int(index - self._start_indices[part_index])
-            return self.arrays[part_index][part_key]
+            return self.parts[part_index][part_index]
         else:
             raise NotImplementedError
 
     def __len__(self) -> int:
-        return sum(len(part) for part in self.arrays)
+        return sum(len(part) for part in self.parts)
 
     def __iter__(self) -> Iterator[T]:
         for i in range(len(self)):
