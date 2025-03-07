debd6ab44b4d62bd27fd0fc71e6027f6095bfc3a
Daniel Dourvaris <dourvaris@gmail.com>
2025-03-07 18:08:11 +0200

    Add versioning support

--- src/pupil_labs/video/__init__.py
+++ src/pupil_labs/video/__init__.py
@@ -1,5 +1,12 @@
 """pupil_labs.video"""
 
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
+
 from pupil_labs.video.array_like import ArrayLike
 from pupil_labs.video.frame import AudioFrame, PixelFormat, ReaderFrameType, VideoFrame
 from pupil_labs.video.indexing import Indexer
@@ -8,6 +15,7 @@ from pupil_labs.video.reader import Reader
 from pupil_labs.video.writer import Writer
 
 __all__: list[str] = [
+    "__version__",
     "ArrayLike",
     "AudioFrame",
     "MultiReader",
