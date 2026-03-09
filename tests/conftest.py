b80eeffd1acbab0890c05aeef2fa21b40ee5fdd8
Daniel Dourvaris <dourvaris@gmail.com>
2026-03-09 09:44:50 +0200

    feat: handle video with corrupt out of order packets and frames (#13)

--- tests/conftest.py
+++ tests/conftest.py
@@ -46,6 +46,7 @@ def pytest_generate_tests(metafunc: Any) -> None:
         TEST_DATA_PATH / "PI Eye Camera.mp4",
         TEST_DATA_PATH / "PI Scene Camera - audio off.mp4",
         TEST_DATA_PATH / "out_of_order_pts.mp4",
+        TEST_DATA_PATH / "way_out_of_order_pts.mp4",
         TEST_DATA_PATH / "duplicate_pts.mp4",
         TEST_DATA_PATH / "silently_skipped_corrupt_frame.mp4",
     ]
